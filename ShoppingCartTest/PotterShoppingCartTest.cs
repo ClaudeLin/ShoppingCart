5773f6f1211006f9bfcfcf35a9f10b593ebe6541
ClaudeLin <linc27595@gmail.com>
2017-06-21 15:32:55 +0800

    加入專案檔案。

---
+++ ShoppingCartTest/PotterShoppingCartTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCartTest
+{
+	[TestClass]
+	public class PotterShoppingCartTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
