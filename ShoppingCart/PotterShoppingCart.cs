dfce793560f3217f14974fdd65e0f2a36e17c9db
ClaudeLin <linc27595@gmail.com>
2017-06-21 16:25:24 +0800

    refactoring after foruth test pass

--- ShoppingCart/PotterShoppingCart.cs
+++ ShoppingCart/PotterShoppingCart.cs
@@ -7,30 +7,25 @@ using System.Xml.Schema;
 
 namespace ShoppingCart
 {
-    public class PotterShoppingCart
-    {
-	    private List<Book> _books;
+	public class PotterShoppingCart
+	{
+		private List<Book> _books;
+		private readonly Dictionary<int, double> _discountTable = new Dictionary<int, double>()
+		{
+			{1,1},
+			{2,0.95},
+			{3,0.9},
+			{4,0.8}
+		};
 
-	    public void AddCommodity(List<Book> book)
-	    {
-		    _books = book;
-	    }
+		public void AddCommodity(List<Book> book)
+		{
+			_books = book;
+		}
 
-	    public double CalculatePrice()
-	    {
-		    if (_books.Count == 2)
-		    {
-			    return _books.Sum(p => p.Price)*0.95;
-		    }
-		    if (_books.Count == 3)
-		    {
-			    return _books.Sum(p => p.Price)*0.9;
-		    }
-		    if (_books.Count == 4)
-		    {
-			    return 320.0;
-		    }
-		    return 100;
-	    }
-    }
+		public double CalculatePrice()
+		{
+			return _books.Sum(p => p.Price) * _discountTable[_books.Count];
+		}
+	}
 }
