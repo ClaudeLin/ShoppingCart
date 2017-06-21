4bf58c0c0ac877800ef6872eb2ca0c590969522b
ClaudeLin <linc27595@gmail.com>
2017-06-21 16:00:39 +0800

    Add Second Test

--- ShoppingCartTest/PotterShoppingCartTest.cs
+++ ShoppingCartTest/PotterShoppingCartTest.cs
@@ -11,7 +11,7 @@ namespace ShoppingCartTest
 		[TestMethod]
 		public void Buy_1_First_Volume_should_pay_100()
 		{
-			var target=new PotterShoppingCart();
+			var target = new PotterShoppingCart();
 			var book = new List<Book>
 			{
 				new Book
@@ -21,8 +21,34 @@ namespace ShoppingCartTest
 				}
 			};
 			target.AddCommodity(book);
-			var actual=target.CalculatePrice();
+			var actual = target.CalculatePrice();
 			var expected = 100;
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Buy_1_First_Volume_and_1_Second_Volume_should_pay_190()
+		{
+			var target = new PotterShoppingCart();
+			var books = new List<Book>
+			{
+				new Book()
+				{
+					VolumeNo = 1,
+					Price = 100
+				},
+				new Book()
+				{
+					VolumeNo = 2,
+					Price = 100
+				}
+			};
+
+			target.AddCommodity(books);
+
+			var actual = target.CalculatePrice();
+			var expected = 190;
 
 			Assert.AreEqual(expected,actual);
 		}
